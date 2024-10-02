17fc113118652d380706cc66ddde06f6bc355706
enveremincaglar <enveremin@proton.me>
2024-10-02 21:59:07 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// Arrays
+string[] answerKeys = new string[5] { "a", "b", "c", "d", "e" };
+string[] userAnswers = new string[5];
+
+// Variables
+int correctAnswersNumber = 0;
+int incorrectAnswersNumber = 0;
+int emptyAnswerNumber = 0;
+int examScore;
+
+// User Input Section
+for (int i = 0; i < answerKeys.Length; i++)
+{
+    Console.Write($"Enter {i + 1}. Answer: ");
+    string userAnswer = Console.ReadLine();
+    userAnswers[i] = userAnswer;
+}
+
+// Compare Answers Loop
+for (int i = 0; i < answerKeys.Length; i++)
+{
+    if (answerKeys[i] == userAnswers[i])
+    {
+        correctAnswersNumber++;
+    }
+    else if (userAnswers[i] == string.Empty)
+    {
+        emptyAnswerNumber++;
+    }
+    else
+    {
+        incorrectAnswersNumber++;
+    }
+}
+
+// Calculate Exam Score
+examScore = correctAnswersNumber - (incorrectAnswersNumber / 3);
+
+// Output
+Console.Clear();
+Console.WriteLine($"Correct Answer Number:  \t{correctAnswersNumber}");
+Console.WriteLine($"Incorrect Answer Number:\t{incorrectAnswersNumber}");
+Console.WriteLine($"Empty Answer Number:    \t{emptyAnswerNumber}");
+Console.WriteLine($"Your Exam Net is:       \t{examScore}");
---
+++ README.md
@@ -0,0 +1,13 @@
+# Exam Scoring Program
+
+## Overview
+This program allows users to input their answers to a series of questions and compares them against predefined correct answers. It calculates the total number of correct, incorrect, and empty answers, as well as an exam score based on the user's performance.
+
+## Features
+- Users can input answers to a set of predefined questions.
+- The program compares user answers with the correct answers.
+- It calculates:
+  - The number of correct answers.
+  - The number of incorrect answers.
+  - The number of unanswered questions.
+  - A net exam score, deducting points for incorrect answers.
